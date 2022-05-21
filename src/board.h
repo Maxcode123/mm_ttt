c80e65a3cae60b5eb3c9d9bd33a3903c039b4ba9
Maxcode123 <nikiforos@live.co.uk>
2022-05-21 22:58:49 +0300

    board methods header

--- src/board.h
+++ src/board.h
@@ -7,4 +7,7 @@ class Board
 public:
     std::vector<Position*> X;
     std::vector<Position*> O;
+    void add_X(Position* x);
+    void add_O(Position* o);
+    void random_position(Position* pos);
 };
